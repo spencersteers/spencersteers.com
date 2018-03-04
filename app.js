17ad9fa686bc85a2cde7b2564cb5c01f0a5b1681
Spencer Steers <spencersteers@gmail.com>
2018-03-03 20:54:32 -0700

    ramp shader, fade out old pixels, make text look right on mobile

--- app.js
+++ app.js
@@ -6,71 +6,139 @@ var MOUSEY = 0;
 var WINDOWHALFX = window.innerWidth / 2;
 var WINDOWHALFY = window.innerHeight / 2;
 
-
 var camera;
+var camGroup;
 var renderer;
 var scene;
 var composer;
 var textComposer;
 var textScene;
+var textMesh;
+var renderTarget;
+var fadeMaterial;
 
+GrayscaleRampShader = {
+
+	uniforms: {
+		"tDiffuse": { type: "t", value: null },
+		"opacity":  { type: "f", value: 1.0 }
+	},
+
+	vertexShader: `
+		varying vec2 vUv;
+		void main() {
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+		}
+	`,
+
+	fragmentShader: `
+		uniform float opacity;
+		uniform sampler2D tDiffuse;
+		varying vec2 vUv;
+
+		void main() {
+			vec3 c1 = vec3(255, 235, 235);
+			vec3 c2 = vec3(244, 220, 217);
+			vec3 c3 = vec3(198, 191, 210);
+			vec3 c4 = vec3(114, 108, 145);
+			// vec3 c5 = vec3(63, 52, 70);
+			vec3 c5 = vec3(0, 0, 0);
+			vec4 texel = texture2D( tDiffuse, vUv );
+			
+			vec3 use = vec3(0.0, 0.0, 0.0);
+			float c = (texel.r + texel.g + texel.b) / 3.0;
+			if (c < 0.25) { use = c5; }
+			else if (c < 0.4) { use = c4; }
+			else if (c < 0.6) { use = c3; }
+			else if (c < 0.8) { use = c2; }
+			else if (c < 1.0) { use = c1; }
+			texel.r = use.r / 256.0;
+			texel.g = use.g / 256.0;
+			texel.b = use.b / 256.0;
+			gl_FragColor = opacity * texel;
+		}
+	`
+};
 
 if (Detector.webgl) {
   init();
   animate();
+  startMouseStoppedTimer();
 }
-else
-  $("#nowebgl").show();
-
-
+else {
+  document.getElementById('nowebgl').style.display = 'block';
+}
 
 window.addEventListener('resize', onWindowResize, false);
 document.addEventListener('mousemove', onDocumentMouseMove, false);
 
 function init() {
-  
   renderer = new THREE.WebGLRenderer();
   renderer.autoClearColor = false;
   renderer.setClearColor(0x000000, 0);
   renderer.setSize(WIDTH, HEIGHT);
   
-  var $container = $('#container');
-  $container.append(renderer.domElement);
+  var container = document.getElementById('container');
+  container.append(renderer.domElement);
 
   scene = new THREE.Scene();
-  // scene.fog = new THREE.Fog(0x000000, 1, 1000);
-
-
-
+  
+  // create camera
   camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
   camera.position.z = 10;
   camera.position.y = -4;
   scene.add(camera);
+  
+  // make transparent plane
+  fadeMaterial = new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      transparent: true,
+      opacity: 0.01
+  });
+  var fadePlane = new THREE.PlaneGeometry(10, 10);
+  fadeMesh = new THREE.Mesh(fadePlane, fadeMaterial);
+  
+  // place plane in front of camera
+  camera.add(fadeMesh);
+  fadeMesh.position.z = -0.5;  
+  
+  
+  
 
-
-  // textScene = new THREE.Scene();
-  scene.add(draw_text("SPENCER STEERS"));
-
+  // draw text
+  var padding = 0.1;
+  var spencerText = draw_text("SPENCER");
+  spencerText.position.y = padding;
+  var spencerTextHeight = spencerText.geometry.boundingBox.max.y - spencerText.geometry.boundingBox.min.y;
+  var steersText = draw_text("STEERS");
+  steersText.position.y = spencerText.position.y - spencerTextHeight - padding;
+  scene.add(spencerText);
+  scene.add(steersText);
+  
+  // draw particles
   drawParticles();
 
-  // post processing 
-  var parameters = {minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat, stencilBuffer: false};
-  var renderTarget = new THREE.WebGLRenderTarget(WIDTH, HEIGHT, parameters);
-
+  // setup post processing
+  // each frame gets copied to the previous frame to create the particle trails
+  var parameters = {
+    minFilter: THREE.LinearFilter, 
+    magFilter: THREE.LinearFilter, 
+    format: THREE.RGBAFormat, 
+    stencilBuffer: false
+  };
+  renderTarget = new THREE.WebGLRenderTarget(WIDTH * devicePixelRatio, HEIGHT * devicePixelRatio, parameters);
 
   composer = new THREE.EffectComposer(renderer, renderTarget);
   composer.addPass(new THREE.RenderPass(scene, camera));
 
-  var effect1 = new THREE.ShaderPass(THREE.DotScreenShader);
-  effect1.uniforms.scale.value = 1;
-  composer.addPass(effect1);
-
-  var effect2 = new THREE.ShaderPass(THREE.RGBShiftShader);
-  effect2.uniforms.amount.value = 0.0001;
-  effect2.renderToScreen = true;
-  composer.addPass(effect2);
-
+  var copyShader = new THREE.ShaderPass(GrayscaleRampShader);
+  copyShader.renderToScreen = true;
+  composer.addPass(copyShader);
 }
+
+
+function initPostProcessing() {}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -101,9 +169,7 @@ function drawParticles() {
     var pX = Math.random() * 500 - 250,
         pY = Math.random() * 500 - 250,
         pZ = Math.random() * -500 + 100,
-        particle = new THREE.Vertex(
-          new THREE.Vector3(pX, pY, pZ)
-        );
+        particle = new THREE.Vector3(pX, pY, pZ);
 
     // add it to the geometry
     particles.vertices.push(particle);
@@ -120,43 +186,57 @@ function drawParticles() {
 
 function draw_text(text) {
   textGeom = new THREE.TextGeometry(text, {
-    'size': 1,
-    'height': 0.1,
+    'size': 0.8,
+    'height': 0.2,
     'font': 'helvetiker',
     'weight': 'normal',
     'style': 'normal',
-    bevelThickness: 0.1,
-    bevelSize: 0.1,
+    bevelThickness: 1,
+    bevelSize: 1,
     bevelEnabled: false,
     material: 0,
     extrudeMaterial: 1
   });
 
   materialArray = [
-    new THREE.MeshBasicMaterial({ color: 0x000000, transparent: true, opacity: 1  }),
-    new THREE.MeshBasicMaterial({ color: 0xFFFFFF, shading: THREE.SmoothShading })
+    new THREE.MeshBasicMaterial({ color: 0xFFFFFF, transparent: true, opacity: 1  }),
+    new THREE.MeshBasicMaterial({ color: 0x444444, shading: THREE.SmoothShading })
   ];
 
   whiteWireframe =  new THREE.MeshBasicMaterial({ color: 0xFFFFFF, transparent: true, opacity: 0.0 });
 
   faceMaterial = new THREE.MeshFaceMaterial(materialArray);
-  text = new THREE.Mesh(textGeom, faceMaterial);
+  textMesh = new THREE.Mesh(textGeom, faceMaterial);
 
   textGeom.computeBoundingBox();
   var textWidth = textGeom.boundingBox.max.x - textGeom.boundingBox.min.x;
   
-  text.position.set(-0.5 * textWidth, 0, 0);
+  textMesh.position.set(-0.5 * textWidth, 0, 0);
+  textMesh.rotation.x = -20 * Math.PI / 180;
 
-  text.rotation.x = -20 * Math.PI / 180;
-
-  return text;
+  return textMesh;
 }
 
+var mouseStoppedTimer;
+function startMouseStoppedTimer() {
+  clearTimeout(mouseStoppedTimer);
+  mouseStoppedTimer = setTimeout(onMouseStopped, 2000);
+}
 
+function onMouseStopped() {
+  // composer.reset();
+}
 
 function onDocumentMouseMove(event) {
+  // 
+  // fadeMaterial.opacity = 0.05;
+  
+  
   MOUSEX = (event.clientX - WINDOWHALFX) / 200;
   MOUSEY = (event.clientY - WINDOWHALFY) / 200;
+  
+  // reset mouse stopped timer
+  startMouseStoppedTimer();
 }
 
 function onWindowResize() {
@@ -168,4 +248,10 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderTarget.setSize(window.innerWidth * devicePixelRatio, window.innerHeight * devicePixelRatio);
+  composer.reset();
 }
+
+
+
+
