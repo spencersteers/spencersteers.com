c11478d577b8ff5b9e4fd2636d6ae5ce99d9169c
Spencer Steers <spencersteers@gmail.com>
2018-09-02 12:47:05 -0600

    video in blog header

--- src/templates/BlogPostTemplate.js
+++ src/templates/BlogPostTemplate.js
@@ -24,14 +24,41 @@ class BlogPostTemplate extends React.Component {
       >
         <header
           style={{
-            height: 540,
+            height: 230,
             paddingTop: rhythm(1),
             background: '#1f2228',
             borderBottom: '1px solid #17191d',
             textAlign: 'center'
           }}
         >
-          <video src="/out3.mp4" muted autoPlay loop playsInline width="252" height="336"/>
+          <div
+            style={{
+              height: '100%',
+              backgroundColor: '#262931',
+              paddingLeft: 20,
+              paddingRight: 20,
+              paddingTop: 20,
+              paddingBottom: 0,
+              display: 'inline-block',
+              margin: '2 auto',
+              borderLeft: '1px solid rgb(23, 25, 29)',
+              borderTop: '1px solid rgb(23, 25, 29)',
+              borderRight: '1px solid rgb(23, 25, 29)',
+              borderRadius: '5px 5px 0 0',
+              position: 'relative',
+              overflow: 'hidden'
+            }}
+          >
+            <video src="/screen_cropped.mp4" muted autoPlay loop playsInline width="300" />
+            <div style={{
+              position: 'absolute',
+              width: '100%',
+              height: 0,
+              boxShadow: '0 -4px 4px 4px rgba(0, 0, 0, 0.75)',
+              left: 0,
+              right: 0
+            }} />
+          </div>
         </header>
         <Layout location={this.props.location}>
           <BlogPost post={post} />
